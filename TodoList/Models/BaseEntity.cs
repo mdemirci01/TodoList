011f6121684aa33ec04b3271dd5476288fccef22
Murat Demirci <mdemirci@outlook.com>
2017-07-10 16:37:38 +0300

    categories module

--- TodoList/Models/BaseEntity.cs
+++ TodoList/Models/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,10 @@ namespace TodoList.Models
     public class BaseEntity
     {
         public int Id { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTime CreateDate { get; set; }
         public string CreatedBy { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTime UpdateDate { get; set; }
         public string UpdatedBy { get; set; }
     }
