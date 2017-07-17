b912b061c613a47958bb9b56a625716f6a3b2b5e
Onur Ağıcı <onuragici@slnyazilim.com>
2017-07-17 17:57:06 +0300

    seed

--- TodoList/Migrations/Configuration.cs
+++ TodoList/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace TodoList.Migrations
 {
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Threading.Tasks;
+    using TodoList.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TodoList.Models.ApplicationDbContext>
     {
@@ -15,6 +18,20 @@ namespace TodoList.Migrations
 
         protected override void Seed(TodoList.Models.ApplicationDbContext context)
         {
+            if (!context.Categories.Any())
+            {
+                context.Categories.Add(new Models.Category() { Name = "Deneme", CreateDate = DateTime.Now, CreatedBy = "username", UpdateDate = DateTime.Now, UpdatedBy = "username" });
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new ApplicationUserManager(store);
+                var user = new Models.ApplicationUser() { Email = "sibel.hatipoglu@sencard.com.tr", UserName = "sibel.hatipoglu@sencard.com.tr" };
+                Task<Microsoft.AspNet.Identity.IdentityResult> task = Task.Run(() => manager.CreateAsync(user, "TodoList123+"));
+
+                // Will block until the task is completed...
+                var result = task.Result;
+                
+
+                context.SaveChanges();
+            }
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
